7927cfcc84ccad5dac8f3312c482fc465aa1b212
yash patel <yash@curlycode.in>
2026-06-10 15:58:48 +0530

    solving bugs

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan; 
 
+Route::get('/run-my-database-setup-secret', function () {
+    Artisan::call('migrate:fresh', ['--seed' => true, '--force' => true]);
+    return "Database successfully migrated and seeded!";
+});
 Route::get('/', function () {
     return view('welcome');
 });
